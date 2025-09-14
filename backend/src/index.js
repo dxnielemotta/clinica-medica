96971de97798c4ad71e51f2c0a7aff074e080b9a
Daniele <danielemottarocha@gmail.com>
2025-09-13 21:55:41 -0300

    update CORS configuration and use env config in frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,12 @@ const app = express();
 
 dbConnection();
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || "http://localhost:5173", // Permite o localhost e a URL de produção
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,26 +1,25 @@
-
-import axios from 'axios';
-import { useUserStore } from '@/stores/user'; 
+import axios from 'axios'
+import { useUserStore } from '@/stores/user'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // url base do backend
-});
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api', // url base do backend
+})
 
 // roda antes de cada requisição ser enviada
 api.interceptors.request.use(
   (config) => {
-    const userStore = useUserStore();
-    const token = userStore.token; // pega o token da store do Pinia
+    const userStore = useUserStore()
+    const token = userStore.token // pega o token da store do Pinia
 
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`
     }
 
-    return config;
+    return config
   },
   (error) => {
-    return Promise.reject(error);
-  }
-);
+    return Promise.reject(error)
+  },
+)
 
-export default api;
+export default api
