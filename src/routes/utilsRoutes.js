0897e7c2dc8d1132ed3c3a22cab936518d61321e
Daniele <danielemottarocha@gmail.com>
2025-09-13 21:35:38 -0300

    moving backend structure to backend folder

--- src/routes/utilsRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/utilsRoutes.js
-import { Router } from "express";
-import { getCepInfo } from "../controllers/utilsController.js";
-
-const router = Router();
-
-router.get("/cep/:cep", getCepInfo);
-
-export default router;
